38d313993f50b58197cbbd72ebc2d26668a2e634
DaneilPei <peixq1222@icloud.com>
2019-05-04 00:34:30 +0800

    change theme

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../src/'))
 
 
@@ -57,3 +58,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
